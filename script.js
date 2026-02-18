adb58ef146dd52780a4edc8644359dc1cd230a86
yocefalaa1-dot <yocefalaa1@gmail.com>
2026-02-18 21:00:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://youssefybrix-ybrix-api.hf.space/chat';
+const API_URL='/api/chat';
 const IMAGE_API = 'https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell';
 
 let chatHistory = [];
